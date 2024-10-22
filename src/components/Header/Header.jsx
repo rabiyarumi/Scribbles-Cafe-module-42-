e0c4f39a16229a94737caccbc70f56c5a930e3dd
rabiyarumi <rabiyaarumi@gmail.com>
2024-10-22 21:41:44 +0600

    added initial set up & fetch the data

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Knowlde Cafe</h1>
+    </div>
+  );
+};
+
+export default Header;
